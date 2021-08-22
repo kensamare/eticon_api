7b98ac03d37e143ea70481a4c6a781a4671291c8
Andrew Kurakov <sniperaft@gmail.com>
2021-08-22 17:51:20 +0700

    test assert

--- lib/eticon_api.dart
+++ lib/eticon_api.dart
@@ -1,5 +1,23 @@
 library eticon_api;
 
+
+class Api {
+  Api._();
+
+  static Api instance = Api._();
+
+  static String? _baseUrl;
+
+  static bool _globalTestMode = false;
+
+  ///Sets the base URL for processing requests
+  void setBaseUrl(String url){
+    assert(url.startsWith('http') || url.startsWith('http'), 'The url should start with https or http');
+    print('URLLLLL');
+  }
+
+}
+
 /// A Calculator.
 class Calculator {
   /// Returns [value] plus 1.
