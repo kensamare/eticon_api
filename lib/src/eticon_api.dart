85977cfcc5adee4cda1053e757cef88c57d35123
kurakov@eticon.ru <kurakov@eticon.ru>
2021-09-02 12:28:28 +0700

    Test new struct

--- lib/src/eticon_api.dart
+++ lib/src/eticon_api.dart
@@ -6,6 +6,8 @@ import 'package:http/http.dart' as http;
 import 'package:http/http.dart' show Response;
 import 'package:shared_preferences/shared_preferences.dart';
 
+class n{}
+
 class Api {
   ///Initialization API class
   static void init(
