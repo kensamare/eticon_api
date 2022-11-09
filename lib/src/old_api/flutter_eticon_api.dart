fbc1c296ee520ca3405e1bae657028085372f91b
Andrew Kurakov <kurakov@eticon.ru>
2022-11-09 15:19:28 +0700

    Dio test

--- lib/src/old_api/flutter_eticon_api.dart
+++ lib/src/old_api/flutter_eticon_api.dart
@@ -1,28 +1,34 @@
-import 'package:eticon_api/eticon_api.dart';
-import 'package:eticon_api/src/old_api/api_st.dart';
 import 'package:eticon_api/src/token.dart';
 import 'package:eticon_api/src/type.dart';
 import 'package:get_storage/get_storage.dart';
 
+import 'api_errors.dart';
+import 'api_st.dart';
+
+
 class OldApi {
   ///Initialization API class
   static Future<void> init(
-      {required String baseUrl,
-      bool globalTestMode = false,
-      bool bearerToken = true,
-      disableAllTestMode = false,
-      bool enableUtf8Decoding = false,
-      bool loadTokenFromMemory = true,
-      String? authTitle,
-      String? storageUrl}) async {
+      {required List<String> urls,
+        bool globalTestMode = false,
+        bool bearerToken = true,
+        disableAllTestMode = false,
+        bool enableUtf8Decoding = false,
+        bool loadTokenFromMemory = true,
+        String? authTitle,
+        String? storageUrl}) async {
     if (!ApiST.instance.setInitState()) {
       throw EticonApiError(error: 'API class already initialization');
     }
-    if (baseUrl.isEmpty) {
-      throw EticonApiError(error: 'URL is empty');
+    if (urls.isEmpty) {
+      throw EticonApiError(error: 'URLs list is empty');
     }
-    if (!baseUrl.startsWith('http')) throw EticonApiError(error: 'The url should start with https or http');
-    if (baseUrl[baseUrl.length - 1] != '/') baseUrl += '/';
+    if (!urls[0].startsWith('http')) throw EticonApiError(error: 'The url should start with https or http');
+    for (int i = 0; i < urls.length; i++) {
+      if (!urls[i].endsWith('/')){
+        urls[i] += '/';
+      }
+    }
     await GetStorage.init();
 
     ///LoadTokenFromMemory now here
@@ -36,8 +42,8 @@ class OldApi {
         Token.instance.setRefreshToken(token);
       }
       String expireTimeString = GetStorage().read('ApiEticonMainExpireDate2312') ?? '';
-      if(expireTimeString.isNotEmpty){
-      DateTime expireTime = DateTime.parse(GetStorage().read('ApiEticonMainExpireDate2312'));
+      if (expireTimeString.isNotEmpty) {
+        DateTime expireTime = DateTime.parse(GetStorage().read('ApiEticonMainExpireDate2312'));
         Token.instance.expireDate = expireTime;
       }
     } else {
@@ -47,7 +53,7 @@ class OldApi {
     if (authTitle != null) {
       ApiST.instance.setAuthTitle(authTitle);
     }
-    ApiST.instance.setBaseUrl(baseUrl);
+    ApiST.instance.setUrls(urls);
     ApiST.instance.setGlobalTestMode(globalTestMode);
     ApiST.instance.disableAllTestMode(disableAllTestMode);
     ApiST.instance.enableUtf8Decoding(enableUtf8Decoding);
@@ -57,7 +63,7 @@ class OldApi {
     }
   }
 
-  static get baseUrl => ApiST.instance.baseUrl;
+  // static get baseUrl => ApiST.instance.baseUrl;
 
   ///Help to get url to resource in server storage
   static String dataFromStorage(String path) {
@@ -140,8 +146,12 @@ class OldApi {
 
   /// Sends an HTTP GET request.
   static Future<Map<String, dynamic>> get(
-      {required String method, bool isAuth = false, bool testMode = false, Map<String, dynamic>? query}) async {
-    if (ApiST.instance.baseUrl == null) {
+      {required String method,
+        bool isAuth = false,
+        bool testMode = false,
+        Map<String, dynamic>? query,
+        int urlIndex = 0}) async {
+    if (ApiST.instance.urls.isEmpty) {
       throw EticonApiError(error: 'Base url not set, use Api.init()');
     }
     if (isAuth) {
@@ -150,116 +160,136 @@ class OldApi {
       }
     }
     return await ApiST.instance
-        .request(type: TYPE.GET, method: method, isAuth: isAuth, testMode: testMode, query: query);
+        .request(type: TYPE.GET, method: method, isAuth: isAuth, testMode: testMode, query: query, urlIndex: urlIndex);
   }
 
   /// Sends an HTTP POST request.
   static Future<Map<String, dynamic>> post(
-      {required String method, bool isAuth = false, bool testMode = false, required Map<String, dynamic> body}) async {
-    if (ApiST.instance.baseUrl == null) {
-      throw EticonApiError(error: 'Base url not set, use Api.init()');
-    }
-    if (isAuth) {
-      if (Token.instance.token.isEmpty) {
-        throw EticonApiError(error: 'Authentication token is empty, use Api.setToken (String url)');
-      }
+      {required String method,
+        bool isAuth = false,
+        bool testMode = false,
+        required Map<String, dynamic> body,
+        int urlIndex = 0}) async {
+    String error = _checkBeforeRequest(isAuth, urlIndex);
+    if (error.isNotEmpty) {
+      throw EticonApiError(error: error);
     }
     return await ApiST.instance
-        .request(type: TYPE.POST, method: method, isAuth: isAuth, testMode: testMode, query: body);
+        .request(type: TYPE.POST, method: method, isAuth: isAuth, testMode: testMode, query: body, urlIndex: urlIndex);
   }
 
   /// Sends an HTTP PUT request.
   static Future<Map<String, dynamic>> put(
-      {required String method, bool isAuth = false, bool testMode = false, required Map<String, dynamic> body}) async {
-    if (ApiST.instance.baseUrl == null) {
-      throw EticonApiError(error: 'Base url not set, use Api.init()');
-    }
-    if (isAuth) {
-      if (Token.instance.token.isEmpty) {
-        throw EticonApiError(error: 'Authentication token is empty, use Api.setToken (String url)');
-      }
+      {required String method,
+        bool isAuth = false,
+        bool testMode = false,
+        required Map<String, dynamic> body,
+        int urlIndex = 0}) async {
+    String error = _checkBeforeRequest(isAuth, urlIndex);
+    if (error.isNotEmpty) {
+      throw EticonApiError(error: error);
     }
     return await ApiST.instance
-        .request(type: TYPE.PUT, method: method, isAuth: isAuth, testMode: testMode, query: body);
+        .request(type: TYPE.PUT, method: method, isAuth: isAuth, testMode: testMode, query: body, urlIndex: urlIndex);
   }
 
   /// Sends an HTTP DELETE request.
   static Future<Map<String, dynamic>> delete(
-      {required String method, bool isAuth = false, bool testMode = false, Map<String, dynamic>? query}) async {
-    if (ApiST.instance.baseUrl == null) {
-      throw EticonApiError(error: 'Base url not set, use Api.init(String url)');
-    }
-    if (isAuth) {
-      if (Token.instance.token.isEmpty) {
-        throw EticonApiError(error: 'Authentication token is empty, use Api.setToken (String url)');
-      }
+      {required String method,
+        bool isAuth = false,
+        bool testMode = false,
+        Map<String, dynamic>? query,
+        int urlIndex = 0}) async {
+    String error = _checkBeforeRequest(isAuth, urlIndex);
+    if (error.isNotEmpty) {
+      throw EticonApiError(error: error);
     }
     return await ApiST.instance
-        .request(type: TYPE.DEL, method: method, isAuth: isAuth, testMode: testMode, query: query);
+        .request(type: TYPE.DEL, method: method, isAuth: isAuth, testMode: testMode, query: query, urlIndex: urlIndex);
   }
 
   /// Sends an HTTP PATCH request.
   static Future<Map<String, dynamic>> patch(
-      {required String method, bool isAuth = false, bool testMode = false, required Map<String, dynamic> body}) async {
-    if (ApiST.instance.baseUrl == null) {
-      throw EticonApiError(error: 'Base url not set, use Api.init()');
-    }
-    if (isAuth) {
-      if (Token.instance.token.isEmpty) {
-        throw EticonApiError(error: 'Authentication token is empty, use Api.setToken (String url)');
-      }
+      {required String method,
+        bool isAuth = false,
+        bool testMode = false,
+        required Map<String, dynamic> body,
+        int urlIndex = 0}) async {
+    String error = _checkBeforeRequest(isAuth, urlIndex);
+    if (error.isNotEmpty) {
+      throw EticonApiError(error: error);
     }
     return await ApiST.instance
-        .request(type: TYPE.PATCH, method: method, isAuth: isAuth, testMode: testMode, query: body);
+        .request(type: TYPE.PATCH, method: method, isAuth: isAuth, testMode: testMode, query: body, urlIndex: urlIndex);
   }
 
   /// Sends an HTTP GET request.
   static Future<Map<String, dynamic>> rawGet(
       {required String url,
-      Map<String, String> headers = const {"Content-type": 'application/json'},
-      bool testMode = false,
-      Map<String, dynamic>? query}) async {
-    return await ApiST.instance
-        .request(type: TYPE.GET, baseUrl: url, rawHeaders: headers, testMode: testMode, query: query);
+        Map<String, String> headers = const {"Content-type": 'application/json'},
+        bool testMode = false,
+        Map<String, dynamic>? query,
+        int urlIndex = 0}) async {
+    return await ApiST.instance.request(
+        type: TYPE.GET, baseUrl: url, rawHeaders: headers, testMode: testMode, query: query, urlIndex: urlIndex);
   }
 
   /// Sends an HTTP POST request.
   static Future<Map<String, dynamic>> rawPost(
       {required String url,
-      Map<String, String> headers = const {"Content-type": 'application/json'},
-      bool testMode = false,
-      required Object body}) async {
-    return await ApiST.instance
-        .request(type: TYPE.POST, baseUrl: url, rawHeaders: headers, testMode: testMode, query: body);
+        Map<String, String> headers = const {"Content-type": 'application/json'},
+        bool testMode = false,
+        required Object body,
+        int urlIndex = 0}) async {
+    return await ApiST.instance.request(
+        type: TYPE.POST, baseUrl: url, rawHeaders: headers, testMode: testMode, query: body, urlIndex: urlIndex);
   }
 
   /// Sends an HTTP PUT request.
   static Future<Map<String, dynamic>> rawPut(
       {required String url,
-      Map<String, String> headers = const {"Content-type": 'application/json'},
-      bool testMode = false,
-      required Map<String, dynamic> body}) async {
-    return await ApiST.instance
-        .request(type: TYPE.PUT, baseUrl: url, rawHeaders: headers, testMode: testMode, query: body);
+        Map<String, String> headers = const {"Content-type": 'application/json'},
+        bool testMode = false,
+        required Map<String, dynamic> body,
+        int urlIndex = 0}) async {
+    return await ApiST.instance.request(
+        type: TYPE.PUT, baseUrl: url, rawHeaders: headers, testMode: testMode, query: body, urlIndex: urlIndex);
   }
 
   /// Sends an HTTP DELETE request.
   static Future<Map<String, dynamic>> rawDelete(
       {required String url,
-      Map<String, String> headers = const {"Content-type": 'application/json'},
-      bool testMode = false,
-      Map<String, dynamic>? query}) async {
-    return await ApiST.instance
-        .request(type: TYPE.DEL, baseUrl: url, rawHeaders: headers, testMode: testMode, query: query);
+        Map<String, String> headers = const {"Content-type": 'application/json'},
+        bool testMode = false,
+        Map<String, dynamic>? query,
+        int urlIndex = 0}) async {
+    return await ApiST.instance.request(
+        type: TYPE.DEL, baseUrl: url, rawHeaders: headers, testMode: testMode, query: query, urlIndex: urlIndex);
   }
 
   /// Sends an HTTP PUT request.
   static Future<Map<String, dynamic>> rawPatch(
       {required String url,
-      Map<String, String> headers = const {"Content-type": 'application/json'},
-      bool testMode = false,
-      required Map<String, dynamic> body}) async {
-    return await ApiST.instance
-        .request(type: TYPE.PATCH, baseUrl: url, rawHeaders: headers, testMode: testMode, query: body);
+        Map<String, String> headers = const {"Content-type": 'application/json'},
+        bool testMode = false,
+        required Map<String, dynamic> body,
+        int urlIndex = 0}) async {
+    return await ApiST.instance.request(
+        type: TYPE.PATCH, baseUrl: url, rawHeaders: headers, testMode: testMode, query: body, urlIndex: urlIndex);
+  }
+
+  static String _checkBeforeRequest(bool isAuth, int index) {
+    if (ApiST.instance.urls.isEmpty) {
+      return 'Base url not set, use Api.init(String url)';
+    }
+    if (ApiST.instance.urls.length <= index) {
+      return 'The index is greater than the number of urls in the list';
+    }
+    if (isAuth) {
+      if (Token.instance.token.isEmpty) {
+        return 'Authentication token is empty, use Api.setToken (String url)';
+      }
+    }
+    return '';
   }
 }
